f80d0868d3b8918c670473020876e24b7d7059a8
Hamza Kristof <77848693+Kristofka995@users.noreply.github.com>
2021-01-24 18:36:37 +0100

    Add files via upload

---
+++ save2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+import joblib
+
+# music_data = pd.read_csv('music.csv')
+# X = music_data.drop(columns=['genre'])
+# y = music_data['genre']
+
+# model = DecisionTreeClassifier()
+# model.fit(X, y)
+
+model = joblib.load( 'music-recommender.joblib')
+
+predictions = model.predict([[21, 1]])
+predictions
